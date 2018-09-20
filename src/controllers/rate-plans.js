ceebb44504511311b4841474840a170188f6c46c
Jirka Chadima <chadima.jiri@gmail.com>
2018-09-20 13:18:44 +0200

    feat(rate-plans): Returns 404 when no rate plan document exists

--- src/controllers/rate-plans.js
+++ src/controllers/rate-plans.js
@@ -3,11 +3,13 @@ const { Http404Error } = require('../errors');
 const findAll = async (req, res, next) => {
   try {
     let plainHotel = await res.locals.wt.hotel.toPlainObject(['ratePlansUri']);
+    if (!plainHotel.dataUri.contents.ratePlansUri) {
+      return next(new Http404Error('ratePlanNotFound', 'Rate plan not found'));
+    }
     let ratePlans = plainHotel.dataUri.contents.ratePlansUri.contents;
     for (let ratePlanId in ratePlans) {
       ratePlans[ratePlanId].id = ratePlanId;
     }
-    // TODO handle hotels without rate plans uri
     res.status(200).json(ratePlans);
   } catch (e) {
     next(e);
@@ -18,13 +20,15 @@ const find = async (req, res, next) => {
   let { ratePlanId } = req.params;
   try {
     let plainHotel = await res.locals.wt.hotel.toPlainObject(['ratePlansUri']);
+    if (!plainHotel.dataUri.contents.ratePlansUri) {
+      return next(new Http404Error('ratePlanNotFound', 'Rate plan not found'));
+    }
     const ratePlans = plainHotel.dataUri.contents.ratePlansUri.contents;
     let ratePlan = ratePlans[ratePlanId];
     if (!ratePlan) {
       return next(new Http404Error('ratePlanNotFound', 'Rate plan not found'));
     }
     ratePlan.id = ratePlanId;
-    // TODO handle hotels without rate plans uri
     res.status(200).json(ratePlan);
   } catch (e) {
     next(e);
